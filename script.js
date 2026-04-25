fb1ecab6f6ab8137daa4ed72c516150e769648f3
rxtx32 <ritushree.2301216@srec.ac.in>
2026-04-25 14:59:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-// Dark mode toggle
 const toggle = document.getElementById("darkModeToggle");
+
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-});
-
-// Form submission (AJAX)
-const form = document.getElementById("contactForm");
-const successMsg = document.getElementById("successMsg");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const data = new FormData(form);
-
-  const response = await fetch(form.action, {
-    method: "POST",
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
-
-  if (response.ok) {
-    successMsg.style.display = "block";
-    form.reset();
-  } else {
-    alert("❌ Error sending message");
-  }
 });
